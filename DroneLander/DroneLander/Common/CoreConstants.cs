b77dbc04b075062cf4fc6f8046cb8ffce88b1b8e
Luan You <Luan.You@beca.com>
2017-06-17 15:56:15 +1200

    Added Mobile Center crash and analytics support

--- DroneLander/DroneLander/Common/CoreConstants.cs
+++ DroneLander/DroneLander/Common/CoreConstants.cs
@@ -17,4 +17,10 @@ namespace DroneLander.Common
         public const double StartingFuel = 1000.0;
         public const double StartingThrust = 0.0;
     }
+
+    public static class MobileCenterConstants
+    {
+        public const string AndroidAppId = "87cb1480-8873-4939-8c39-4ef328e8958c";
+        public const string iOSAppId = "";
+    }
 }
